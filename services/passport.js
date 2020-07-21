7bed304ab8834c0833f883b43d789c978f2fb4d8
vishwanath gugwad <you@example.com>
2020-07-21 16:36:15 +0530

    production

---
+++ services/passport.js
@@ -0,0 +1,35 @@
+const passport = require("passport");
+const GoogleStratergy = require("passport-google-oauth20").Strategy;
+const keys = require("../Config/keys");
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
+passport.use(
+  new GoogleStratergy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then((user) => done(null, user));
+        }
+      });
+    }
+  )
+);
